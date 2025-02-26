a2760249824a61706c5023c2d73b0aff7eebf4e0
ワロフズ、 <paitoon.j@outlook.co.th>
2025-02-26 09:59:10 +0700

    Update main.sh

--- rootfs/installer/main.sh
+++ rootfs/installer/main.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# Determine Architecture
-ARCH=$(uname -m)
-
-## Honeygain
-mkdir -p /opt/honeygain 
-if [[ "$ARCH" == "x86_64" ]]
-then 
-    curl -L https://github.com/Walofz/CashReaper/releases/download/0.0.1/0.8.1x64.tar.gz | tar -C /opt/honeygain -zxf -
-else
-    curl -L https://github.com/Walofz/CashReaper/releases/download/0.0.1/0.8.1_arm64.tar.gz | tar -C /opt/honeygain -zxf -
-fi
-
-
-# Pawns.app (IPRoyal)
-curl -L https://cdn.pawns.app/download/cli/latest/linux_`uname -m`/pawns-cli > /usr/bin/pawns-cli
-chmod +x /usr/bin/pawns-cli
-
-# Earnapp - Installed by service itself on first run
+# Honeygain - Installed by dockerfile
+# Earnapp
+wget -qO- https://brightdata.com/static/earnapp/install.sh > /tmp/earnapp.sh && bash /tmp/earnapp.sh -y
+rm /tmp/earnapp.sh
